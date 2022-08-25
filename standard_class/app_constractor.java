f285ebcf0d40e5916d1dd0f36085bb435cc141dd
brando allen <111488269+badong0@users.noreply.github.com>
2022-08-25 15:48:35 +0800

    Create app_constractor.java

---
+++ standard_class/app_constractor.java
@@ -0,0 +1,15 @@
+public class app {
+public static void main(String[] args) {
+
+        // Character Class
+    character knight = new character(10, 5, 0, "Chen");
+
+
+    character mage = new character(0, 5, 10, "Akasha");
+
+
+    character thief = new character(5, 10, 0, "Gondar");
+
+
+    }
+}
