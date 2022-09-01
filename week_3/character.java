f8d8bf34c0ac533cccea4e77a68e8d96dfa26682
brando allen <111488269+badong0@users.noreply.github.com>
2022-09-01 15:50:05 +0800

    Create character.java

---
+++ week_3/character.java
@@ -0,0 +1,12 @@
+public class character {
+    // Character attributes
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
+    
+        // Character method
+    public void sayMyName() {
+        System.out.println("Hello I am " + name + " My strenght is " +strength + " My agility is " + agility + " My intelligence is " + intelligence);
+    }
+}
