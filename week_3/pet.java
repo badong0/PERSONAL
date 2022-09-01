f7c900d8e99c299c5d2eb92c9ab2935c0e55d812
brando allen <111488269+badong0@users.noreply.github.com>
2022-09-01 15:51:45 +0800

    Create pet.java

---
+++ week_3/pet.java
@@ -0,0 +1,15 @@
+public class pet {
+
+    int price;
+    float weight;
+    int quantity;
+    String description;
+    boolean canbetraded;
+ 
+     // pet
+    public void petCompanion() {
+        System.out.println("This is: " + description + " weights " + weight + " Quantity " + quantity + " Can be Traded? " + canbetraded);
+        
+    }
+}
+//this is without constractor
