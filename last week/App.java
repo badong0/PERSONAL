2b298a1ed10ab431960e6f0e254daa410e999e09
brando allen <111488269+badong0@users.noreply.github.com>
2022-11-03 16:57:09 +0800

    Create App.java
    mahirap ito, check patterns on multi dimention array

---
+++ last week/App.java
@@ -0,0 +1,298 @@
+public class App{
+    public static void main(String[] args){
+        char[][] mdArray= 
+        {
+        {'S','L','A','M'},
+        {'T','I','L','E'},
+        {'E','A','T','S'},
+        {'P','R','O','S'}
+        };
+        
+        // //ROWS WORDS
+        // //PATTERN 1
+        // System.out.println("");
+        // System.out.println("1. CHAR OF PATTERN 1.");
+        // char row1 = 0;
+        // for (int i=0;i<mdArray.length; i++){
+        //     for (int j=0;j<mdArray.length;j++){
+        //     if (i == 0)
+        //     System.out.print(mdArray[0][j]);
+        //     }
+        // }
+        // System.out.print(row1);
+
+        //         //PATTERN 1
+        // System.out.println("");
+        // System.out.println("1. CHAR OF PATTERN 1.");
+        // char row2 = 0;
+        // for (int i=0;i<mdArray.length; i++){
+        //     for (int j=0;j<mdArray.length;j++){
+        //     if (i == 0)
+        //     System.out.print(mdArray[1][j]);
+        //     }
+        // }
+        // System.out.print(row2);
+
+        // //PATTERN 1
+        // System.out.println("");
+        // System.out.println("1. CHAR OF PATTERN 1.");
+        // char row3 = 0;
+        // for (int i=0;i<mdArray.length; i++){
+        //     for (int j=0;j<mdArray.length;j++){
+        //     if (i == 0)
+        //     System.out.print(mdArray[2][j]);
+        //     }
+        // }
+        // System.out.print(row3);
+
+        // //PATTERN 1
+        // System.out.println("");
+        // System.out.println("1. CHAR OF PATTERN 1.");
+        // char row4 = 0;
+        // for (int i=0;i<mdArray.length; i++){
+        //     for (int j=0;j<mdArray.length;j++){
+        //     if (i == 0)
+        //     System.out.print(mdArray[3][j]);
+        //     }
+        // }
+        // System.out.print(row4);
+
+
+
+
+
+        // // COLUMS WORDS
+        // // PATTERN 2
+        // System.out.println("");
+        // System.out.println("2. CHAR OF PATTERN 2");
+        // char column0 = 0;
+        // for (int i=0;i<mdArray.length; i++){
+        //     for (int j=0;j<mdArray.length;j++){
+        //     if (j == 0)
+        //     System.out.print(mdArray[i][j]); 
+        //     }
+        // }
+        // System.out.print(column0);
+
+        // // PATTERN 2
+        // System.out.println("");
+        // System.out.println("2. CHAR OF PATTERN 2");
+        // char column1 = 0;
+        // for (int i=0;i<mdArray.length; i++){
+        //     for (int j=0;j<mdArray.length;j++){
+        //     if (j == 1)
+        //     System.out.print(mdArray[i][1]); 
+        //     }
+        // }
+        // System.out.print(column1);
+
+        // // PATTERN 2
+        // System.out.println("");
+        // System.out.println("2. CHAR OF PATTERN 2");
+        // char column2 = 0;
+        // for (int i=0;i<mdArray.length; i++){
+        //     for (int j=0;j<mdArray.length;j++){
+        //     if (j == 2)
+        //     System.out.print(mdArray[i][2]); 
+        //     }
+        // }
+        // System.out.print(column2);
+
+        // // PATTERN 2
+        // System.out.println("");
+        // System.out.println("2. CHAR OF PATTERN 2");
+        // char column3 = 0;
+        // for (int i=0;i<mdArray.length; i++){
+        //     for (int j=0;j<mdArray.length;j++){
+        //     if (j == 2)
+        //     System.out.print(mdArray[i][3]); 
+        //     }
+        // }
+        // System.out.print(column3);
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////
+    //     // PATTERN 4 
+    //     System.out.println("SITS pattern");
+    //     System.out.println("4. SUM OF PATTERN 4                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  ");
+    //     char diagonal1 = 0;
+    //     for (int i=0;i<mdArray.length; i++){
+    //         for (int j=0;j<mdArray.length;j++){
+
+    //             if(i==0&&j==0||i==1&&j==1||i==2&&j==2||i==3&&j==3){
+
+    //             System.out.print(mdArray[i][j]);
+    //             }
+    //         }
+    //     }    
+    //     System.out.print(diagonal1);  
+
+        
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+        // //PATTERN 1
+        // System.out.println("");
+        // System.out.println("");
+        // char row1 = 0;
+        // for (char i=0;i<mdArray.length; i++){
+        //     for (char j=0;j<mdArray.length;j++){
+        //         if (i==3&&j==0||i==2&&j==1||i==1&&j==2||i==0&&j==3)
+        //         System.out.print(mdArray[i][j]);
+        //     }
+        // }
+        // System.out.print(row1);
+
+        // //PATTERN 1
+        // System.out.println("Pro");
+        // System.out.println("");
+        // char scramble1 = 0;
+        // for (char i=0;i<4; i++){
+        //     for (char j=0;j<3;j++){
+                
+        //         if (i==3&&j==0||i==3&&j==1||i==3&&j==2)
+        //         System.out.print(mdArray[i][j]);
+        //     }
+        // }
+        // System.out.print(scramble1);
+
+        // //PATTERN 1
+        // System.out.println("eat");
+        // System.out.println("");
+        // char scramble1 = 0;
+        // for (char i=0;i<4; i++){
+        //     for (char j=0;j<3;j++){
+                
+        //         if (i==2&&j==0||i==2&&j==1||i==2&&j==2)
+        //         System.out.print(mdArray[i][j]);
+        //     }
+        // }
+        // System.out.print(scramble1);
+
+        // //PATTERN 1
+        // System.out.println("sat");
+        // System.out.println("");
+        // char scramble1 = 0;
+        // for (char i=0;i<4; i++){
+        //     for (char j=0;j<3;j++){
+                
+        //         if (i==0&&j==0||i==2&&j==1||i==2&&j==2)
+        //         System.out.print(mdArray[i][j]);
+        //     }
+        // }
+        // System.out.print(scramble1);
+
+        // //PATTERN 1
+        // System.out.println("sea");
+        // System.out.println("");
+        // char scramble1 = 0;
+        // for (char i=0;i<4; i++){
+        //     for (char j=0;j<3;j++){
+                
+        //         if (i==0&&j==0||i==2&&j==1||i==2&&j==0)
+        //         System.out.print(mdArray[i][j]);
+        //     }
+        // }
+        // System.out.print(scramble1);
+
+        // //PATTERN 1
+        // System.out.println("set");
+        // System.out.println("");
+        // char scramble1 = 0;
+        // for (char i=0;i<4; i++){
+        //     for (char j=0;j<3;j++){
+                
+        //         if (i==0&&j==0||i==2&&j==0||i==2&&j==2)
+        //         System.out.print(mdArray[i][j]);
+        //     }
+        // }
+        // System.out.print(scramble1);
+
+        // //PATTERN 1
+        // System.out.println("lat");
+        // System.out.println("");
+        // char scramble1 = 0;
+        // for (char i=0;i<4; i++){
+        //     for (char j=0;j<3;j++){
+                
+        //         if (i==1&&j==2||i==2&&j==1||i==2&&j==2)
+        //         System.out.print(mdArray[i][j]);
+        //     }
+        // }
+        // System.out.print(scramble1);
+
+        //PATTERN 1
+        // System.out.println("aet");
+        // System.out.println("");
+        // char scramble1 = 0;
+        // for (char i=0;i<4; i++){
+        //     for (char j=0;j<3;j++){
+                
+        //         if (i==2&&j==0||i==2&&j==2||i==0&&j==2)
+        //         System.out.print(mdArray[i][j]);
+        //     }
+        // }
+        // System.out.print(scramble1);
+
+    // //    PATTERN 1
+    //     System.out.println("ato");
+    //     System.out.println("");
+    //     char scramble1 = 0;
+    //     for (char i=0;i<4; i++){
+    //         for (char j=0;j<3;j++){
+                
+    //             if (i==3&&j==2||i==2&&j==2||i==0&&j==2)
+    //             System.out.print(mdArray[i][j]);
+    //         }
+    //     }
+    //     System.out.print(scramble1);
+
+    // //    PATTERN 1
+    //     System.out.println("tit");
+    //     System.out.println("");
+    //     char scramble1 = 0;
+    //     for (char i=0;i<3; i++){
+    //         for (char j=0;j<3;j++){
+                
+    //             if (i==1&&j==1||i==1&&j==0||i==2&&j==2)
+    //             System.out.print(mdArray[i][j]);
+    //         }
+    //     }
+    //     System.out.print(scramble1);
+
+    //    PATTERN 1
+        // System.out.println("tie");
+        // System.out.println("");
+        // char scramble1 = 0;
+        // for (char i=0;i<3; i++){
+        //     for (char j=0;j<3;j++){
+                
+        //         if (i==1&&j==1||i==1&&j==0||i==2&&j==0)
+        //         System.out.print(mdArray[i][j]);
+        //     }
+        // }
+        // System.out.print(scramble1);
+
+
+    // //    PATTERN 1
+    //     System.out.println("sil");
+    //     System.out.println("");
+    //     char scramble1 = 0;
+    //     for (char i=0;i<3; i++){
+    //         for (char j=0;j<3;j++){
+                
+    //             if (i==1&&j==1||i==1&&j==2||i==0&&j==0)
+    //             System.out.print(mdArray[i][j]);
+    //         }
+    //     }
+    //     System.out.print(scramble1);
+    }
+}
